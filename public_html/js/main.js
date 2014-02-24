8bf7b0f88fc62a678c09f765a59cdc08ca6d28f1
James Stiehl <jstiehl@James-Stiehls-MacBook-Pro.local>
2014-02-23 23:09:33 -0800

    adding new files for todo app

---
+++ public_html/js/main.js
@@ -0,0 +1,79 @@
+//Add click handler for the button
+
+ var tasks = [{Description:"Drink Beer", done: false},
+              {Description:"Watch TV", done: false},
+              {Description:"Read a book", done: false},
+              {Description:"Mow the lawn", done: false},
+              {Description:"Eat dinner", done: false}
+              ];
+
+
+$(function () { // on ready!
+
+
+
+ 	$('#submit-btn').click(function(e){
+    e.preventDefault();
+
+    var newTask = $("#exampleInputTask").val();
+    if (newTask === ''){
+      alert("No task was entered");
+    } else {
+    
+    var postData = {
+      Description: newTask,
+      done: false
+    }
+    
+    tasks.push(postData);
+    addTasks(postData);
+
+    $.post("/api/data", JSON.stringify(tasks), function(data){
+      console.log("Success!");
+    });
+  
+    $("#exampleInputTask").val('');
+  }
+  });
+ 
+  // $.getJSON("/api/data", function(data){
+  //   renderDetails(data);
+  // }
+  
+  renderDetails();
+
+});
+
+function renderDetails () {
+  $table = $("#to-do-table");
+  $("#sample-task").addClass("hidden");
+  tasks.forEach(function(item){
+    addTasks(item);
+  });
+}
+
+function addTasks(item) {   
+    $row = $('<tr></tr>');
+    $cell = $('<td></td>');
+    $theCheck = $('<input type=\"checkbox\">');
+    $deleteButton = $('<button type=\"submit\" class=\"btn btn-default\">Delete</button>');
+    $table.append($row.append($cell.append($theCheck)));
+    $row.append('<td>'+item.Description+'</td>');
+    $row.append($('<td></td>').append($deleteButton));
+
+    $theCheck.click(function(){
+      if(!item.done){
+        console.log("Megan is a Jerk!");
+        item.done = true;
+        $(this).parent().parent().addClass("completed");
+      } else {
+        item.done = false;
+        $(this).parent().parent().removeClass("completed");
+      }
+    });
+
+    $deleteButton.click(function(){
+      $(this).parent().parent().addClass("hidden");
+    });
+  
+}
---
+++ server.js
@@ -0,0 +1,69 @@
+var http = require('http');
+var url  = require('url');
+var path = require('path');
+var fs   = require('fs');
+
+var mime = require('mime');
+
+var server = http.createServer(router).listen(7357);
+
+function router (req, res) {
+  var pathname = url.parse(req.url, true).pathname;
+  if (pathname.slice(0, 4) === '/api') {
+    apiHandler(req, res);
+  } else {
+    if (pathname[pathname.length - 1] === '/') 
+      pathname += 'index.html';
+    staticFileHandler(pathname, res);
+  }
+}
+
+function staticFileHandler (pathname, res) {
+  fs.readFile(__dirname + '/public_html' + pathname, function (err, data) {
+    if (err) return errHandler(err, res);
+    console.log('[200]: ' + pathname);
+    res.setHeader('Content-Type', mime.lookup(path.extname(pathname)));
+    res.end(data);
+  });
+}
+
+function errHandler (err, res) {
+  if (err.code === 'ENOENT') {
+    res.statusCode = 404;
+    res.end('File not found!');
+    console.log('[404]: File not found: ' + err.path);
+  } else {
+    console.error(err);
+  }
+}
+
+function apiHandler (req, res) {
+  if (req.method === 'GET') {
+    var pathname = url.parse(req.url).pathname
+    if (pathname === '/api/data') {
+      fs.readFile("data/data.json", {encoding: 'utf8'}, function(err,data){
+        if(err) return errHandler(err,res);
+        try {
+        data = JSON.stringify(JSON.parse(data)); // this will check to make sure json file is formatted correctly
+        } catch (err){
+          data = "{\"name\":\"Error\", \"email\":[\"error@error.error\"]}";
+        }
+        res.setHeader('Content-Type', mime.lookup('json'));
+        res.end(data);
+      });
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
+  } else if (req.method === "POST"){
+    req.pipe(fs.createWriteStream('data/data.json'));
+    res.end();
+  }
+}
+
+
+
+
+
+
+

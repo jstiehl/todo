e14408d755866fab2ff2c4eb3bb3db8452d54bb5
Jason O'Brien <nilsobrien@gmail.com>
2015-11-25 11:44:00 -0600

    fixing package file

--- server.js
+++ server.js
@@ -7,45 +7,6 @@ var mime = require('mime');
 var server = http.createServer(router).listen(process.env.PORT || 5000);
 Parse.initialize("D7copRkkZPKmIbFvELzkaxwGAp95DRSyvbOJa7Z5", "rl4dixTWclxNLVMXWPkxliUN9vU9A6mEMM4lMKpc");
 
-// Sample of looping through parse user list
-/*
- var query = new Parse.Query(Parse.user);
- query.find({
- success: function(usernameers) {
- for (var i = 0; i < users.length; ++i) {
- console.log(users[i].get('username'));
- }
- }
- });
- */
-/*
- //sample of querying for specific user
- var query = new Parse.Query(Parse.User);
- var ParseUser = new Parse.User
- query.equalTo("username", "James");  // find all the women
- query.find({
- success: function(user) {
- // Do stuff
- if (user.length > 0){
-
- ParseUser = user[0];
-
- }
-
- console.log(ParseUser.username);
- /*
- for (var i = 0; i < user.length; i++) {
- var object = user[i];
- console.log(object.id + ' - ' + object.createdAt);
- }
-
-
-
- }
- });
-
- */
-
 function router (req, res) {
   var pathname = url.parse(req.url, true).pathname;
   if (pathname.slice(0, 4) === '/api') {
@@ -81,9 +42,10 @@ function apiHandler (req, res) {
     //send back a list of todos
     // var toDo = new Parse.Object("ToDo");
     var parseQuery = new Parse.Query("ToDo");
+    console.log(req.headers);
     parseQuery.find({
       success: function(toDoList){
-          res.setHeader('Content-Type', mime.lookup('json'));
+        res.setHeader('Content-Type', mime.lookup('json'));
         res.end(JSON.stringify(toDoList));
       },
       error: function(toDoList, error) {
@@ -93,10 +55,9 @@ function apiHandler (req, res) {
     });
 
   } else if (req.method === "POST"){
-
-    var body = "";
-    req.on('data', function (chunk) {
-      body += chunk;
+      var body = "";
+      req.on('data', function (chunk) {
+        body += chunk;
     });
 
     var today = new Date();
@@ -105,7 +66,7 @@ function apiHandler (req, res) {
       var toDo = new Parse.Object("ToDo");
       toDo.set('Description', body);
       toDo.set('Done', false);
-      toDo.set('DueDate',today )
+
       toDo.save(null, {
         success: function(toDo) {
           // Execute any logic that should take place after the object is saved.
